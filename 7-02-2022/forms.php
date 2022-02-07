d04bdab2d57b14b0611a7c54912c409872ded322
govind-avvaru <98160328+govind-avvaru@users.noreply.github.com>
2022-02-07 19:04:21 +0530

    Create forms.php

---
+++ 7-02-2022/forms.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title> forms</title>
+</head>
+<body>
+<form action="javascript.php?myurl=mnnjkvnjds" method="post">
+    <input type="text" name="uname" placeholder="enter your name"/> <br> <br>
+
+    <input type="email" name="uemail" placeholder="enter your email"/> <br> <br>
+
+    <input type="submit" value="submit"/>
+
+
+
+</form>
+</body>
+</html>
+
