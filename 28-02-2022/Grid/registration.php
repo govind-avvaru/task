09c31b17c2cb5da8557f61b95573702292f0a30e
govind-avvaru <98160328+govind-avvaru@users.noreply.github.com>
2022-02-28 18:55:05 +0530

    Add files via upload

---
+++ 28-02-2022/Grid/registration.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * I95dev_Grid Module Registration
+ *
+ * @category    I95dev
+ * @package     I95dev_Grid
+ * @author      I95dev Software Private Limited
+ *
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'I95dev_Grid',
+    __DIR__
+);
+?>
