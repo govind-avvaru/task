7c7688c613457f8c4871fcc4f50e99c3598771ad
govind-avvaru <98160328+govind-avvaru@users.noreply.github.com>
2022-03-04 19:16:56 +0530

    Add files via upload

---
+++ Customform/Block/ContactForm.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace I95dev\Customform\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class ContactForm extends \Magento\Contact\Block\ContactForm
+{
+    public function helloContactForm()
+    {
+        return "helloContactForm says hi!";
+    }
+
+    public function helloContactFormTwo()
+    {
+        return "helloContactFormTwo says hi!";
+    }
+}
+?>
