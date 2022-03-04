f50021981ce0cf06a72594e05ad80d8c721e07d2
govind-avvaru <98160328+govind-avvaru@users.noreply.github.com>
2022-03-04 19:47:10 +0530

    Update registration.php

--- CustomPaymentOption/registration.php
+++ CustomPaymentOption/registration.php
@@ -2,7 +2,7 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'CCBill_CustomPaymentOption',
+    'I95dev_CustomPaymentOption',
     __DIR__
 );
 ?>
